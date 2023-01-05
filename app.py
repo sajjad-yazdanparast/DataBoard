5ad3c2fd5de1cbe31341a1bef67f5f861c2b2967
sajjad-yazdanparast <sajjad.yazdanparast.tehrani@gmail.com>
2023-01-05 04:34:49 +0330

    UDPDATE app.py to contain local databases

--- app.py
+++ app.py
@@ -6,24 +6,27 @@ st.set_page_config(
     page_title='DataBoard' ,
     page_icon= plt.imread(os.path.abspath('static/logo.png')),
 )
-st.sidebar.title('DataBases')
 
-cnx = get_connection(user=st.secrets.db_username, password=st.secrets.db_password, host=st.secrets.db_host)
 
-if 'databases' not in st.session_state :
+if st.checkbox('Use remote database?') :
+    cnx = get_connection(user=st.secrets.db_username, password=st.secrets.db_password, host=st.secrets.db_host)
     databases = [db[0] for db in get_database_names(cnx)]
     st.session_state.databases = databases
+else :
+    cnx = get_connection(user='sajjad_quera', password="P@ssword1401", host='localhost')
+    databases = [db[0] for db in get_database_names(cnx)]
+    st.session_state.databases = databases
+
 
 def on_table_radio_change(db_name) :
     if db_name in st.session_state :
         table_selected = st.session_state.get(db_name)
-        st.session_state.table_selected = table_selected
+        st.subheader(f'{table_selected} table from {db_name} database dashboarding')
         st.dataframe(get_table_in_database(cnx,db_name = db_name, table_name= table_selected))
 
 
-
-
-with st.sidebar :
+st.sidebar.title('DataBases')
+with st.sidebar:
     for db in st.session_state.databases :
         with st.expander(db) :
             tables = get_all_tables_in_database(cnx, db)
